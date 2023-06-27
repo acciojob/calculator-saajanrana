4ab4b5324bfb2fc3559087515158f0fd0bf3db65
Saajan Singh Rana <123057148+saajanrana@users.noreply.github.com>
2023-06-27 05:16:36 -0400

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,21 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const display = document.getElementById("display");
-  const buttons = document.querySelectorAll("button");
+let input = document.getElementById('input');
+let expression = "";
+function solve(e) {
+    // console.log(e)
+    if (e === 'ans') {
+        // evaluate the expression
+        let result = eval(expression);
+        console.log(result);
+        input.value = result;
+    }
 
-  buttons.forEach(button => {
-    button.addEventListener("click", function() {
-      const value = button.innerText;
-      const currentDisplay = display.value;
-
-      if (value === "C") {
-        display.value = "";
-      } else if (value === "&larr;") {
-        display.value = currentDisplay.slice(0, -1);
-      } else if (value === "=") {
-        try {
-          display.value = evaluateExpression(currentDisplay);
-        } catch (error) {
-          display.value = "Error";
-        }
-      } else {
-        display.value += value;
-      }
-    });
-  });
-
-  function evaluateExpression(expression) {
-    // Handle open and close brackets
-    expression = expression.replace(/op/g, "(").replace(/cl/g, ")");
-
-    // Evaluate the expression
-    return eval(expression);
-  }
-});
+    else if (e === 'clear') {
+        expression = "";
+        input.value = ""
+    }
+    else {
+        expression += e;
+        console.log(expression)
+        input.value = expression;
+    }
+}
