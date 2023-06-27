575c52c4d88143b7d554447b3572d9beb4d02e71
Saajan Singh Rana <123057148+saajanrana@users.noreply.github.com>
2023-06-27 04:37:30 -0400

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your code here
+document.querySelectorAll("#calculator button").forEach(button => {
+  button.addEventListener("click", handleButtonClick);
+});
+
+// Function to handle button clicks
+function handleButtonClick(event) {
+  const buttonId = event.target.id;
+  const display = document.getElementById("display");
+  
+  switch (buttonId) {
+    case "C":
+      // Clear the display
+      display.value = "";
+      break;
+    case "back":
+      // Remove the last character from display
+      display.value = display.value.slice(0, -1);
+      break;
+    case "equal":
+      // Evaluate the expression and display the result
+      try {
+        display.value = eval(display.value);
+      } catch (error) {
+        display.value = "Error";
+      }
+      break;
+    default:
+      // Append the button value to the display
+      display.value += buttonId;
+      break;
+  }
+}
